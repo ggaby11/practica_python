0413d86e67bc9b4b7447511ea08b0982759afb19
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-02-12 21:32:32 -0500

    agregar 11_sorting_hat

---
+++ 11_sorting_hat.py
@@ -0,0 +1,4 @@
+question_one = input("Q1) Do you like Dawn or Dusk? \n    1) Dawn\n    2) Dusk\n")
+question_two = input("Q2) Do you like Dawn or Dusk? \n    1) Dawn\n    2) Dusk\n")
+question_three = input("Q3) Do you like Dawn or Dusk? \n    1) Dawn\n    2) Dusk\n")
+
