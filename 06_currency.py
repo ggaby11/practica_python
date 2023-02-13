51e64716d1d067360a6c2715765c888e59c70936
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-02-12 19:14:42 -0500

    agregar 06_currency

---
+++ 06_currency.py
@@ -0,0 +1,6 @@
+yuan = float(input("what do u have left in yuan? "))
+yen = float(input("what do u have left in yen? "))
+won = float(input("what do u have left in won? "))
+
+rta = (yuan*0.15) + (yen*0.0077) + (won*0.0008)
+print(round(rta, 4))
