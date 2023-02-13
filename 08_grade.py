93a4a828ee72b6f33177b075985a5ffe8105610b
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-02-12 21:28:03 -0500

    agregar 08_grade

---
+++ 08_grade.py
@@ -0,0 +1,14 @@
+#Here I learned that tab is really important
+import random
+grade = random.randint(50,100)
+
+if grade > 90:
+  print("A")
+elif grade > 80:
+  print("B")
+elif grade > 70:
+  print("C")
+elif grade > 60:
+  print("D")
+else:
+  print("F")  
