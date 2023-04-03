0b8d5c4b3a087bba3bf68a8c7ddb2053bdd65724
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-04-03 16:14:48 -0500

    comentarios funcion round()

--- 06_currency.py
+++ 06_currency.py
@@ -3,4 +3,4 @@ yen = float(input("what do u have left in yen? "))
 won = float(input("what do u have left in won? "))
 
 rta = (yuan*0.15) + (yen*0.0077) + (won*0.0008)
-print(round(rta, 4))
+print(round(rta, 4)) #redondear 4 cifras después de la coma/punto
