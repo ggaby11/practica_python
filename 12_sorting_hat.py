86bd059b08e64e5d26547eecdd87b7dab7619843
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-04-03 15:57:58 -0500

    finalizacion del programa y correccion cantidad al sumar varibles

--- 12_sorting_hat.py
+++ 12_sorting_hat.py
@@ -19,25 +19,34 @@ question_two = int(input("Q2) When I'm dead, I want people to remember me as: ? 
 if question_two > 4:
   print("Wrong input.")
 elif question_two == 4:
-  gryffindor += 1
+  gryffindor += 2
 elif question_two == 3:
-  ravenclaw += 1
+  ravenclaw += 2
 elif question_two == 2:
-  slytherin += 1
+  slytherin += 2
 else: 
-  hufflepuff += 1
+  hufflepuff += 2
 
 question_three = int(input("Q3) Wich kind of instrument most pleases your ear? \n    1) The violin\n    2) The trumpet\n    3) The piano\n    4) The drum\n"))
 
 if question_three > 4:
   print("Wrong input.")
 elif question_three == 4:
-  gryffindor +1
+  gryffindor += 4
 elif question_three == 3:
-  ravenclaw +1
+  ravenclaw += 4
 elif question_three == 2:
-  hufflepuff +1
+  hufflepuff += 4
 else: 
-  slytherin +1
+  slytherin += 4
 
-if(gryffindor > )
+lista = [gryffindor, ravenclaw, hufflepuff, slytherin]
+
+if gryffindor > ravenclaw and ravenclaw > hufflepuff and hufflepuff > slytherin:
+  print(f"gryffindor ({gryffindor})")
+elif ravenclaw > hufflepuff and hufflepuff > slytherin:
+  print(f"ravenclaw ({ravenclaw})")
+elif hufflepuff > slytherin:
+  print(f"hufflepuff ({hufflepuff})")
+else: 
+  print(f"slytherin ({slytherin})")
