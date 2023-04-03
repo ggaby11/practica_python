45e0e1af054ba6d80f2d0b2d8a4a41f5d2126418
ggaby11 <78220603+ggaby11@users.noreply.github.com>
2023-04-03 11:07:08 -0500

    Delete 11_sorting_hat

--- 11_sorting_hat.py
+++
@@ -1,4 +0,0 @@
-question_one = input("Q1) Do you like Dawn or Dusk? \n    1) Dawn\n    2) Dusk\n")
-question_two = input("Q2) Do you like Dawn or Dusk? \n    1) Dawn\n    2) Dusk\n")
-question_three = input("Q3) Do you like Dawn or Dusk? \n    1) Dawn\n    2) Dusk\n")
-
