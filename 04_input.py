f70558cf01c8a505fcbdcebafc885d84adb6e3e8
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-04-03 09:51:25 -0500

    mejoras, uso de listas y random

--- 04_input.py
+++ 04_input.py
@@ -1,5 +1,14 @@
+import random
+
 user_name = (input('Please, enter your name: '))
-print(user_name)
+print(f"Hello, {user_name}")
 
 user_age = int(input('Now, what\'s your age: '))
-print(user_age)
+greetings = ["Hi", "Yo!", "Ahoy", "What's up!", "Hello", "Hey"]
+if(user_age >= 25 and user_age <= 50): 
+    print("Hello fossil =)")
+else: print(random.choice(greetings))
+
+"""otras opciones para seleccion random/secrets
+random.SystemRandom() 
+random.SystemRandom().sample(lista, #) -> selecciona más de 1 element random"""
