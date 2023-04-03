0b533346138b030658425cb65e5ec233bfd7abd9
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-04-03 15:51:01 -0500

    mejoras, agrege variable with_taller_person

--- 11_cyclone.py
+++ 11_cyclone.py
@@ -1,10 +1,16 @@
-print("Welcome to the Cyclone, the best NY city rollercoaster!")
-height = float(input("What's your height? (cm)"))
+print("\nWelcome to the Cyclone, the best NY city rollercoaster! \nPlease answer the next questions to determine if you can enter...")
+height = float(input("\nWhat's your height? (cm) "))
 credits = int(input("How many credits do you have? "))
 
-if height >= 137 and credits >= 10:
+with_taller_person = str(input("Are you with a taller person? (y/n) "))
+
+if with_taller_person == 'y':
+    with_taller_person = True
+else: with_taller_person = False
+
+if (height >= 137 and credits >= 10) or (height >= 100 and with_taller_person):
     print("Enjoy the ride!!")
-elif height < 137:
+elif (height < 137):
     print("I'm sorry, you're not tall enough to ride")
 elif credits < 10:
     print("Ups, you don't have enough credits to ride")
