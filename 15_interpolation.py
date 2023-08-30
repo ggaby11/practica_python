660af41432f22c509d8700f320994799115bdd1a
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-08-30 15:57:37 -0500

    13-16 ejercicios

---
+++ 15_interpolation.py
@@ -0,0 +1,12 @@
+
+i = 100
+while(i >= 2):
+    i = i-1
+    print(f"{i} bottles of beer on the wall, {i} bottles of beer. Take one down, pass it around")
+"""
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('take one down pass it around')
+  print(f'{i-1} bottles of beer on the wall')
+"""
